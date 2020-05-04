494c33a111c667acd127acc9fab0f1837125b620
Aljesh-basnet <58818892+Aljesh-basnet@users.noreply.github.com>
2020-05-04 07:47:31 +0545

    Register

---
+++ user_management/src/main/java/com/coursework/user_management/Signup.java
@@ -0,0 +1,127 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.coursework.user_management;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author pguragain3
+ */
+public class Signup extends HttpServlet {
+    User u = new User();
+    
+    
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+            
+            
+            /* TODO output your page here. You may use following sample code. */
+           
+           u.setUsername(request.getParameter("username"));
+           u.setEmail(request.getParameter("email"));
+           u.setUser_birthdate(request.getParameter("birthdate"));
+           u.setUser_is_admin("false");
+           u.setUser_created_date(java.time.LocalDate.now().toString());
+           u.setUser_blocked_status("false");
+           u.setUser_password1(request.getParameter("password1"));
+           u.setUser_passsword2(request.getParameter("password2"));
+
+           Connection con = MyConnection.getConnection();
+           String query = "insert into userdb (username,email,birthdate,password,userIsAdmin,userCreatedDate,userBlockedStatus) "
+                   + "values (?,?,?,?,?,?,?)";
+         
+           PreparedStatement ps = con.prepareStatement(query);
+           
+           
+           ps.setString(1,u.getUsername());//setting username in the database
+           ps.setString(2,u.getEmail());
+           ps.setString(3,u.getUser_birthdate());
+           ps.setString(4,u.getUser_password1());
+           ps.setString(5,u.getUser_is_admin());
+           ps.setString(6,u.getUser_created_date());
+           ps.setString(7,u.getUser_blocked_status());
+           
+           
+           if(u.username_exists(request.getParameter("username"))==true || u.password_mismatch()==true){
+           out.println("Username exists or password mismatch");
+           }
+           else if(u.empty_fields_detected()==true){
+            out.println("Some fields are empty");
+        }
+           else{
+           ps.executeUpdate();
+           
+           out.println("Working");
+           }
+        } catch (SQLException ex) {
+            Logger.getLogger(Signup.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ user_management/src/main/java/com/coursework/user_management/User.java
@@ -0,0 +1,201 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.coursework.user_management;
+
+/**
+ *
+ * @author aljes
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author aljesh
+ */
+public class User {
+    
+    private int user_id;
+    private String username;
+    private String email;
+    private String user_birthdate;
+    private String user_password1;
+    private String user_password2;
+    private String user_is_admin;
+    private String user_blocked_status;
+    private String user_created_date;  
+    
+    /**
+     * @return the user_id
+     */
+    public int getUser_id() {
+        return user_id;
+    }
+
+    /**
+     * @param user_id the user_id to set
+     */
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the gender
+     */
+    
+
+    /**
+     * @return the user_birthdate
+     */
+    public String getUser_birthdate() {
+        return user_birthdate;
+    }
+
+    /**
+     * @param user_birthdate the user_birthdate to set
+     */
+    public void setUser_birthdate(String user_birthdate) {
+        this.user_birthdate = user_birthdate;
+    }
+
+    /**
+     * @return the user_password1
+     */
+    public String getUser_password1() {
+        return user_password1;
+    }
+
+    /**
+     * @param user_password1 the user_password1 to set
+     */
+    public void setUser_password1(String user_password1) {
+        this.user_password1 = user_password1;
+    }
+
+    /**
+     * @return the user_passsword2
+     */
+    public String getUser_password2() {
+        return user_password2;
+    }
+
+    /**
+     * @param user_password2
+     */
+    public void setUser_passsword2(String user_password2) {
+        this.user_password2 = user_password2;
+    }
+
+    /**
+     * @return the user_is_admin
+     */
+    public String getUser_is_admin() {
+        return user_is_admin;
+    }
+
+    /**
+     * @param user_is_admin the user_is_admin to set
+     */
+    public void setUser_is_admin(String user_is_admin) {
+        this.user_is_admin = user_is_admin;
+    }
+
+    /**
+     * @return the user_blocked_status
+     */
+    public String getUser_blocked_status() {
+        return user_blocked_status;
+    }
+
+    /**
+     * @param user_blocked_status the user_blocked_status to set
+     */
+    public void setUser_blocked_status(String user_blocked_status) {
+        this.user_blocked_status = user_blocked_status;
+    }
+
+    /**
+     * @return the user_created_date
+     */
+    public String getUser_created_date() {
+        return user_created_date;
+    }
+
+    /**
+     * @param user_created_date the user_created_date to set
+     */
+    public void setUser_created_date(String user_created_date) {
+        this.user_created_date = user_created_date;
+    }
+    
+    
+    public boolean username_exists(String username) throws SQLException{  //Code of checking username uniqueness
+        Boolean result = false;
+        String query="select * from userdb where username = ?";
+        
+        PreparedStatement st = MyConnection.getConnection().prepareStatement(query);
+        st.setString(1, username); //username is set on the prepared statement
+        ResultSet r1=st.executeQuery();
+            if(r1.next()) {
+            result = true;
+            }
+        
+        return result;
+}
+    
+    public boolean empty_fields_detected(){
+        if(username.isEmpty() ||email.isEmpty() ||user_birthdate.isEmpty() ||user_password1.isEmpty() ||user_password2.isEmpty()){
+            return true;
+        }
+        else return false;
+    }
+    
+    public boolean password_mismatch(){
+    if(user_password1.equals(user_password2)){
+        return false;
+    }
+    else return true;
+    }
+
+    
+}
